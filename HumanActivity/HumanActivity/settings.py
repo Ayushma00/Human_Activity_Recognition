a6f946013b9ddd112c640b3c92d0b56d4c0bc8af
Aayushma pant <34135400+Ayushma00@users.noreply.github.com>
2024-05-08 23:24:53 +0545

    Update settings.py

--- HumanActivity/HumanActivity/settings.py
+++ HumanActivity/HumanActivity/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'senah%ow_*=9nwtpb1jbe4mjh*&xq$mlh1@p&@-3*@gyx!q182'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
